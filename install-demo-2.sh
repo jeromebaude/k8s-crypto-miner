87069f591ea5066c695086be9aa53bd88533310b
Jerome Baude <jerome.baude@gmail.com>
2022-12-08 14:56:01 +0100

    updating install-demo-2.sh

--- install-demo-2.sh
+++ install-demo-2.sh
@@ -3,7 +3,7 @@
 if [ ! -f /tmp/xmrig-6.15.0/xmrig ]; then
         echo "[INFO] XMRig not found, downloading XMRig now"
         cd /tmp
-        wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+        curl -L https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz > xmrig-6.15.0-linux-static-x64.tar.gz
         tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
         cd xmrig-6.15.0
         chmod +x xmrig
